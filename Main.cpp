1f0dd63aa9d5cf3f991318eaea186c60240f8552
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2018-08-31 13:46:30 -0400

    Modularized parameter parsing logic and expanded command line options.

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
-#include <algorithm>
+// main.cpp
+
 #include <chrono>
 #include <fstream>
 #include <functional>
@@ -7,45 +8,101 @@
 #include <sstream>
 #include <string>
 
-int main(int argc, char ** argv) {
-	long long nSeed;
-	nSeed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-	//nSeed = 0x37373737;
-	std::mt19937_64 oRNG(nSeed);
-	std::uniform_real_distribution<> oDist(100, 999);
-	auto generate = std::bind(oDist, oRNG);
+struct Params {
+	// Good params flag
+	bool good_params;
+
+	// First param
+	long long int entity_count;
+
+	// Second param
 	std::string file_loc;
-	__int64 entity_count;
-	if (argc > 2) {
-		std::stringstream parser;
-		parser << argv[1];
-		parser >> entity_count;
-		file_loc.assign(argv[2]);
 
-		std::ofstream result_file(file_loc, std::ios::binary);
+	// Third param (optional)
+	double uniform_dist_min_inclusive = 100.0;
 
-		__int64 writeCount = 0ll;
-		double bread;
-		char * breadPtr = (char *)&bread;
-		char breadCharArr[9];
-		breadCharArr[8] = 0;
-		for (__int64 i = 0; i < entity_count; i++) {
-			bread = generate();
-			std::copy(breadPtr, breadPtr + 8, breadCharArr);
-			result_file.write((char *)&bread, 8);
+	// Fourth param (optional)
+	double uniform_dist_max_exclusive = 1000.0;
+
+	// Fifth param (optional)
+	long long int seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+};
+
+Params parse_params(int argc, char ** argv) {
+	Params retval;
+
+	if (argc < 3) {
+		retval.good_params = false;
+	}
+
+	/*
+	Must specify both the min and the max.
+	*/
+	if (argc == 4) {
+		retval.good_params = false;
+	}
+
+	else {
+		retval.good_params = true;
+
+		if (argc > 6) {
+			argc = 6;
+		}
+
+		std::stringstream paramParser;
+		for (int i = argc-1; i >= 1; i--) {
+			paramParser << argv[i] << " ";
+		}
+
+		switch (argc) {
+		case 6:
+			paramParser >> retval.seed;
+		case 5:
+			paramParser >> retval.uniform_dist_max_exclusive;
+			paramParser >> retval.uniform_dist_min_inclusive;
+		case 3:
+			paramParser >> retval.file_loc;
+			paramParser >> retval.entity_count;
+			break;
+		}
+	}
+
+	return retval;
+}
+
+int main(int argc, char ** argv) {
+	int exitCode = 0;
+
+	auto userParams = parse_params(argc, argv);
+
+	if (userParams.good_params) {
+		std::cout << "Good params" << std::endl;
+
+		std::mt19937_64 oRNG(userParams.seed);
+		std::uniform_real_distribution<> oDist(userParams.uniform_dist_min_inclusive, userParams.uniform_dist_max_exclusive);
+		auto generate = std::bind(oDist, oRNG);
+
+		std::ofstream result_file(userParams.file_loc, std::ios::binary);
+
+		long long int writeCount = 0ll;
+		double randReal;
+		for (long long int i = 0; i < userParams.entity_count; i++) {
+			randReal = generate();
+			result_file.write((char *)&randReal, 8);
 			writeCount++;
 		}
 
 		result_file.close();
 
-		std::cout << "File " << file_loc << " (" << writeCount << " elements) written." << std::endl;
+		std::cout << "File " << userParams.file_loc << " (" << writeCount << " elements) written." << std::endl;
 	}
 	else {
 		std::cerr << "Bad params" << std::endl;
+		exitCode = -1;
 	}
 
 	char buff[2];
 	std::cin.read(buff, 1);
 
-	return 0;
+	return exitCode;
 }
